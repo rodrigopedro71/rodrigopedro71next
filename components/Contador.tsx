f1b78c1195ea51556d66c5dc46f0bbad5fa07472
rodrigopedro71 <a22407371@alunos.ulht.pt>
2025-11-27 11:42:20 +0000

    Contador com localStorage

--- components/Contador.tsx
+++ components/Contador.tsx
@@ -1,27 +1,70 @@
-import Link from "next/link"
-import { useState } from "react";
+"use client";
 
+import { useEffect, useState } from "react";
 
 export default function Contador() {
+    const [count, setCount] = useState(() => {
+        const countStored = localStorage.getItem("count");
+        return countStored ? parseInt(countStored) : 0;
+    });
 
-    const [count, setCount] = useState<number>(0);
-     const [history, setHistory] = useState<number[]>([]);
+    const [history, setHistory] = useState<number[]>(() => {
+        const historyStored = localStorage.getItem("history");
+        return historyStored ? JSON.parse(historyStored) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("count", String(count));
+        localStorage.setItem("history", JSON.stringify(history));
+    }, [count, history]);
 
     return (
         <>
-        <p>Clicaste {count} vezes</p>
-        
-        <button onClick={() => {
-            const novo = count + 1;
-            setCount(novo);
-            setHistory([...history, novo]);
-        } }
-        >
-            Clica Aqui
-        </button>
-        <p>Historico:
-            {history.map((c, i) => (<span key ={i}>{c}</span>))}
-        </p>
+            <p>Clicaste {count} vezes</p>
+
+            <button
+                onClick={() => {
+                    const novo = count + 1;
+                    setCount(novo);
+                    setHistory((history) => [...history, novo]);
+                }}
+            >
+                Clica Aqui Para Aumentar
+            </button>
+
+            <br />
+
+            <button
+                onClick={() => {
+                    const novo = count - 1;
+                    setCount(novo);
+                    setHistory((history) => [...history, novo]);
+                }}
+            >
+                Clica Aqui Para Diminuir
+            </button>
+
+            <br />
+
+            <button
+                onClick={() => {
+                    const novo = 0;
+                    setCount(novo);
+                    setHistory([novo]);
+                }}
+            >
+                Reset
+            </button>
+
+            <p>
+                Historico:{" "}
+                {history.map((c, i) => (
+                    <span key={i}>
+                        {c}
+                        {i < history.length - 1 ? "; " : ""}
+                    </span>
+                ))}
+            </p>
         </>
-    )
+    );
 }
